df726d5320c04f4187c8c5c6589df69bedf0840a
Prasanta Kakati <prasantakakati1994@gmail.com>
2023-09-27 23:57:21 +0530

    added contact infos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import * as classnames from "classnames";
+import { Send, Linkedin, GitHub, Phone, Mail } from "react-feather";
 
 import Avatar from "@/components/Avatar";
 import TypeWriter from "@/components/TypeWriter";
@@ -28,6 +29,37 @@ export default function Home() {
               infinite={false}
               classNames="position"
             />
+            <div className="contact-buttons d-flex align-items-center">
+              <button className="button contact-button">
+                <Send size={18} className="feather-icon" />
+                Contact Me
+              </button>
+              <a
+                href="https://www.linkedin.com/in/prasantak/"
+                target="_blank"
+                className="contact-link"
+                rel="noreferrer"
+              >
+                <Linkedin size={18} className="feather-icon" />
+              </a>
+              <a
+                href="https://github.com/nullc0der/"
+                target="_blank"
+                className="contact-link"
+                rel="noreferrer"
+              >
+                <GitHub size={18} className="feather-icon" />
+              </a>
+              <a href="tel:+919954707983" className="contact-link">
+                <Phone size={18} className="feather-icon" />
+              </a>
+              <a
+                href="mailto:prasantakakati1994@gmail.com"
+                className="contact-link"
+              >
+                <Mail size={18} className="feather-icon" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="pure-u-1 pure-u-md-1-2 d-flex justify-content-center">
