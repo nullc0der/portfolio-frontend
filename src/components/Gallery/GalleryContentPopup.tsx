b6297b8276e3539e7c58eb407f8f0652b1c3ccd0
Prasanta Kakati <prasantakakati1994@gmail.com>
2023-10-18 02:49:07 +0530

    feat: added animation on gallerycontent show/hide

---
+++ src/components/Gallery/GalleryContentPopup.tsx
@@ -0,0 +1,129 @@
+import classnames from "classnames";
+import { useState, useRef, useEffect } from "react";
+import { ArrowLeft, ArrowRight, X } from "react-feather";
+
+import { type GallerySource } from "@/pages/Projects/projects";
+import { useClickOutside, useSwipe } from "@/lib/hooks";
+import styles from "./Gallery.module.css";
+
+type GalleryContentPopupProps = {
+  currentContent: GallerySource | null;
+  onClickNextPrev: (isNext: boolean) => void;
+  onClose: () => void;
+};
+
+export default function GalleryContentPopup({
+  currentContent,
+  onClose,
+  onClickNextPrev,
+}: GalleryContentPopupProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [showContent, setShowContent] = useState<boolean>(false);
+  const [prevContent, setPrevContent] = useState<GallerySource | null>(null);
+
+  if (prevContent?.id !== currentContent?.id) {
+    if (currentContent) {
+      setShowContent(true);
+    }
+    setPrevContent(currentContent);
+  }
+
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case "Escape":
+          onClose();
+          break;
+        case "ArrowRight":
+          onClickNextPrev(true);
+          break;
+        case "ArrowLeft":
+          onClickNextPrev(false);
+          break;
+        default:
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", onKeyDown);
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  }, [onClickNextPrev, onClose]);
+
+  const onSwipeLeft = () => {
+    onClickNextPrev(true);
+  };
+
+  const onSwipeRight = () => {
+    onClickNextPrev(false);
+  };
+
+  const swipeHandlers = useSwipe(onSwipeLeft, onSwipeRight);
+
+  const hideGalleryContent = () => {
+    if (currentContent) {
+      setShowContent(false);
+    }
+  };
+
+  useClickOutside(ref, hideGalleryContent);
+
+  const onAnimationEnd = () => {
+    if (!showContent) {
+      onClose();
+    }
+  };
+
+  return (
+    <div
+      className={classnames(
+        styles.popupContainer,
+        "d-flex",
+        "align-items-center",
+        "justify-content-center",
+        { show: currentContent, hide: !currentContent }
+      )}
+      {...swipeHandlers}
+    >
+      <div
+        className={classnames("gallery-content", {
+          show: showContent,
+          hide: !showContent,
+        })}
+        ref={ref}
+        onAnimationEnd={onAnimationEnd}
+      >
+        <div className="close-btn" onClick={hideGalleryContent}>
+          <X size={24} />
+        </div>
+        <div
+          className="next-btn"
+          onClick={() => {
+            onClickNextPrev(true);
+          }}
+        >
+          <ArrowRight size={24} />
+        </div>
+        <div
+          className="prev-btn"
+          onClick={() => {
+            onClickNextPrev(false);
+          }}
+        >
+          <ArrowLeft size={24} />
+        </div>
+        {currentContent &&
+          (currentContent.type === "image" ? (
+            <img src={currentContent.src} />
+          ) : (
+            <iframe
+              className="youtube-iframe"
+              src={currentContent.iframeSource}
+              title="YouTube video player"
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,108 +1,16 @@
 import classnames from "classnames";
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
-import { ArrowLeft, ArrowRight, Play, X } from "react-feather";
+import { Play } from "react-feather";
 
 import { type GallerySource } from "@/pages/Projects/projects";
-import { useClickOutside, useSwipe } from "@/lib/hooks";
+import GalleryContentPopup from "./GalleryContentPopup";
 import styles from "./Gallery.module.css";
 
 type GalleryProps = {
   contents: GallerySource[];
   classNames?: string;
 };
-
-type GalleryContentPopupProps = {
-  currentContent: GallerySource;
-  onClickNextPrev: (isNext: boolean) => void;
-  onClose: () => void;
-};
-
-function GalleryContentPopup({
-  currentContent,
-  onClose,
-  onClickNextPrev,
-}: GalleryContentPopupProps) {
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const onKeyDown = (event: KeyboardEvent) => {
-      switch (event.key) {
-        case "Escape":
-          onClose();
-          break;
-        case "ArrowRight":
-          onClickNextPrev(true);
-          break;
-        case "ArrowLeft":
-          onClickNextPrev(false);
-          break;
-        default:
-          break;
-      }
-    };
-
-    document.addEventListener("keydown", onKeyDown);
-    return () => {
-      document.removeEventListener("keydown", onKeyDown);
-    };
-  }, [onClickNextPrev, onClose]);
-
-  useClickOutside(ref, onClose);
-
-  const onSwipeLeft = () => {
-    onClickNextPrev(true);
-  };
-
-  const onSwipeRight = () => {
-    onClickNextPrev(false);
-  };
-
-  const swipeHandlers = useSwipe(onSwipeLeft, onSwipeRight);
-
-  return (
-    <div
-      className={classnames(
-        styles.popupContainer,
-        "d-flex",
-        "align-items-center",
-        "justify-content-center"
-      )}
-      {...swipeHandlers}
-    >
-      <div className="gallery-content" ref={ref}>
-        <div className="close-btn" onClick={onClose}>
-          <X size={24} />
-        </div>
-        <div
-          className="next-btn"
-          onClick={() => {
-            onClickNextPrev(true);
-          }}
-        >
-          <ArrowRight size={24} />
-        </div>
-        <div
-          className="prev-btn"
-          onClick={() => {
-            onClickNextPrev(false);
-          }}
-        >
-          <ArrowLeft size={24} />
-        </div>
-        {currentContent.type === "image" ? (
-          <img src={currentContent.src} />
-        ) : (
-          <iframe
-            className="youtube-iframe"
-            src={currentContent.iframeSource}
-            title="YouTube video player"
-          />
-        )}
-      </div>
-    </div>
-  );
-}
 
 export default function Gallery({ contents, classNames }: GalleryProps) {
   const [currentContent, setCurrentContent] = useState<GallerySource | null>(
@@ -153,18 +61,17 @@ export default function Gallery({ contents, classNames }: GalleryProps) {
           )}
         </div>
       ))}
-      {!!currentContent &&
-        createPortal(
-          <GalleryContentPopup
-            currentContent={currentContent}
-            onClose={() => {
-              setCurrentContent(null);
-            }}
-            onClickNextPrev={onClickNextPrev}
-          />,
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          document.getElementById("portalRoot")!
-        )}
+      {createPortal(
+        <GalleryContentPopup
+          currentContent={currentContent}
+          onClose={() => {
+            setCurrentContent(null);
+          }}
+          onClickNextPrev={onClickNextPrev}
+        />,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        document.getElementById("portalRoot")!
+      )}
     </div>
   );
 }
